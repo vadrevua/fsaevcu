2141a6fa7cebf48b42b0318ec7e334c1b21b40d1
vadrevua <vadrevua@vcu.edu>
2018-01-03 06:06:26 -0500

    added partners page

--- 5.sql
+++
@@ -1,6 +0,0 @@
---5
-select d.department_id, d.department_name, e.salary, count(e.employee_id)
-from employees e join departments d
-on e.department_id = d.department_id
-where e.salary > (select avg(salary) from employees)
-and (select count(department_id) from employees) > 3;
